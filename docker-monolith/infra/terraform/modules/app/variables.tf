3dd6d4732c8e22a31e06259db968319018ccaa54
Lukin Pavel <lukinpvl#gmail.com>
2020-02-20 08:22:41 -0500

    homework #12 with **exersice

---
+++ docker-monolith/infra/terraform/modules/app/variables.tf
@@ -0,0 +1,24 @@
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  description = "Path to the private key user for ssh access"
+}
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+variable instance_count {
+  description = "Instanse count"
+  default     = 1
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
---
+++ docker-monolith/infra/terraform/terraform.tfvars
@@ -0,0 +1,8 @@
+project          = "docker-268712"
+public_key_path  = "~/.ssh/appuser.pub"
+private_key_path = "~/.ssh/appuser"
+disk_image       = "reddit-docker-base"
+region           = "europe-west1"
+zone             = "europe-west1-b"
+#target_tags      = "reddit-docker"
+instance_count   = 1
